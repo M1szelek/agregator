7325c416f3f8bf5f5717c387a178a58f0e04e992
Michal Matlega <m1sz3lek@gmail.com>
2017-01-29 14:53:56 +0100

    Listen port fixed

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.get('/', function(req, res){
 		
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 5000, function(){
 	console.log("Example app listening on port 3000!");
 });
