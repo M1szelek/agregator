279a147d652c902313b2a6d46f2f26625ec73af6
Michal Matlega <m1sz3lek@gmail.com>
2017-01-29 13:47:30 +0100

    Init commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+var cheerio = require('cheerio');
+
+app.get('/', function(req, res){
+	var options = {
+  		host: 'salenauts.com',
+  		port: 80,
+  		path: '/pl/game/shenzhen-io/'
+	};
+
+	var respond;
+
+	http.get(options, function(http_res) {
+  			var data = "";
+
+		    http_res.on("data", function (chunk) {
+		        data += chunk;
+		    });
+
+		    http_res.on("end", function () {
+		        var $ = cheerio.load(data);
+		    	respond = $('.local-price-column').eq(1).text();
+		    	res.send(respond);
+		    });
+
+
+		}).on('error', function(e) {
+  			console.log("Got error: " + e.message);
+		});
+
+		
+});
+
+app.listen(3000, function(){
+	console.log("Example app listening on port 3000!");
+});
