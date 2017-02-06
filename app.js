7be654fa8c25363f9547690095c212574dbea238
Michal Matlega <m1sz3lek@gmail.com>
2017-02-06 23:29:12 +0100

    Promises

--- app.js
+++ app.js
@@ -4,39 +4,51 @@ var app = express();
 
 var cheerio = require('cheerio');
 
-var options = {
-        host: 'salenauts.com',
-        port: 80,
-        path: '/pl/game/shenzhen-io/',
-        res: undefined,
-        data: undefined
-};
+//var render = "";
 
 var games = [
     'shenzhen-io',
     'undertale'
 ]
 
-var sites = [
-    {
-        host: 'salenauts.com',
-        port: 80,
-        path: '/pl/game/shenzhen-io/'
-    }
-]
+var jadiscoOptions = {
+    'host': 'jadisco.pl',
+    'port': 80,
+    'path': '/'
+}
 
 app.get('/', function(req, res){
 
-    //var games = getPagesArray();
+    // var p1 = new Promise(function(resolve, reject) {
+    //   resolve('Success!');
+    //   // or
+    //   // reject ("Error!");
+    // });
 
-    //console.log(games);
+    // p1.then(function(value) {
+    //   console.log(value); // Success!
+    //   res.send(value);
+    // }, function(reason) {
+    //   console.log(reason); // Error!
+    //   res.send(reason);
+    // });
 
+    var games = getPagesArray();
+
+    loadPage(games[1]).then(getBestPrice).then(function(render){
+        res.send(render);
+    });
+
+    
+    
     //games.forEach(function(options){
-   //     loadPage(options)
+    //    loadPage(options, getBestPrice);
     //})
 
-    options.res = res;
-    loadPage(options, getBestPrice);
+    //options.res = res;
+    //loadPage(options, getBestPrice);
+
+    //res.send(render);
 
 });
 
@@ -44,36 +56,73 @@ app.listen(process.env.PORT || 5000, function(){
     console.log("App started!");
 });
 
-var loadPage = function(options, callback){
-    http.get(options, function(http_res) {
-            var data = "";
+var loadPage = function(options){
+    return new Promise(function(resolve, reject){
+            http.get(options, function(http_res) {
+                var data = "";
 
-            http_res.on("data", function (chunk) {
-                data += chunk;
-            });
+                http_res.on("data", function (chunk) {
+                    data += chunk;
+                });
 
-            http_res.on("end", function () {
-                options.data = data;
-                callback(options);
-            });
-    }).on('error', function(e) {
-        options.res.send("Got error: " + e.message);
-    });;
+                http_res.on("end", function () {
+                    resolve(data);
+                });
+        }).on('error', function(e) {
+            reject("Got error: " + e.message);
+        });
+    });
+    
+}
+
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
 }
 
 var getBestPrice = function(data){
-    var $ = cheerio.load(data.data);
+    return new Promise(function(resolve, reject){
+        render = "";
+
+        var $ = cheerio.load(data);
 
                 
-    price = $('.game-title').text() + " ";
-    price += $('.local-price-column').eq(1).text();
-    price += ' <a href="https://salenauts.com' + $('.price-button').eq(2).attr('href') + '">link</a>';
+        render += $('.game-title').text() + " ";
+        render += $('.local-price-column').eq(1).text();
+        render += ' <a href="https://salenauts.com' + $('.price-button').eq(2).attr('href') + '">link</a></br>';
 
-    data.res.send(price);
-}
+        resolve(render);
+    });
 
-var getJadiscoStatus = function(data){
+   
 
+    //data.res.send(price);
 }
 
 var getPagesArray = function(){
@@ -89,36 +138,3 @@ var getPagesArray = function(){
 
     return result;
 }
-
-// var getBestPrice = function(options,res){
-//     var price;
-
-//     http.get(options, function(http_res) {
-//             var data = "";
-
-//             http_res.on("data", function (chunk) {
-//                 data += chunk;
-//             });
-
-            
-
-//             http_res.on("end", function () {
-//                 var $ = cheerio.load(data);
-
-                
-//                 price = $('.game-title').text() + " ";
-//                 price += $('.local-price-column').eq(1).text();
-//                 price += ' <a href="https://salenauts.com' + $('.price-button').eq(2).attr('href') + '">link</a>';
-
-//                 res.send(price);
-//             });
-
-
-
-
-//         }).on('error', function(e) {
-//             console.log("Got error: " + e.message);
-//         });
-
-//     return price;
-// }
